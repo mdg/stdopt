e38b60d84d24ba5fa192aa794284d90fcc16069f
Matthew Graham <mdg149@optonline.net>
2008-12-07 02:24:03 -0500

    fixed some lines that were going past 80 characters. removed old option_c and option_value_list_c classes.  everything left is expected to be kept so should be fixed to compile and tested.

--- include/stdopt/option.h
+++ include/stdopt/option.h
@@ -98,6 +98,11 @@ class option_value_c
 : virtual public option_value_i
 {
 public:
+	typedef typename std::vector< T >::const_iterator iterator;
+private:
+	typedef std::vector< T > value_list;
+
+public:
 	option_value_c();
 	option_value_c( const T &default_value );
 
@@ -127,6 +132,15 @@ public:
 	const T & value( int i ) const;
 
 	/**
+	 * Get the begin iterator for the list of values.
+	 */
+	iterator begin() const { return m_values.begin(); }
+	/**
+	 * Get the end iterator for the list of values.
+	 */
+	iterator end() const { return m_values.end(); }
+
+	/**
 	 * Virtual parser
 	 */
 	virtual bool parse_value( const std::string &str_value )
@@ -145,11 +159,15 @@ public:
 	}
 
 private:
-	std::vector< T > m_values;
+	value_list m_values;
 	bool m_set;
 	bool m_error;
 };
 
+
+/**
+ * An option that can be set on the command line.
+ */
 template < typename T >
 class usage_option_c
 : public option_value_c< T >
@@ -160,19 +178,20 @@ public:
 	 * Construct the config option.  The name is the key in the
 	 * configuration file.
 	 */
-	usage_option_c( char short_opt, const std::string &name, const std::string &desc )
+	usage_option_c( char short_opt, const std::string &name
+			, const std::string &desc )
 	: option_value_c()
 	, m_option_name( name )
 	, m_description( desc )
 	, m_option_char( short_opt )
-	, m_requires_param( false )
 	{}
 
 	/**
 	 * Construct the config option with a default value.  The name
 	 * is the key in the configuration file.
 	 */
-	usage_option_c( const T &default_value, char short_opt, const std::string &name, const std::string &desc )
+	usage_option_c( const T &default_value, char short_opt
+			, const std::string &name, const std::string &desc )
 	: option_value_c( default_value )
 	, m_option_name( name )
 	, m_description( desc )
@@ -181,19 +200,41 @@ public:
 
 
 	virtual char option_character() const { return m_option_char; }
-	virtual const std::string & option_name() const { return m_option_name; }
-	virtual const std::string & description() const { return m_description; }
+	/**
+	 * Get the name of this option.
+	 */
+	virtual const std::string & option_name() const
+	{
+		return m_option_name;
+	}
+	/**
+	 * Get the description for this option.
+	 */
+	virtual const std::string & description() const
+	{
+		return m_description;
+	}
 
-	virtual bool requires_param() const { return usage_option_i::type_requires_param< T >(); }
+	/**
+	 * Check if this option requires a parameter.  The implementation
+	 * is that bool will not require a param, everything else will.
+	 */
+	virtual bool requires_param() const
+	{
+		return usage_option_i::type_requires_param< T >();
+	}
 
 private:
 	std::string m_option_name;
 	std::string m_description;
 
 	char m_option_char;
-	bool m_requires_param;
 };
 
+
+/**
+ * An option to be set in the configuration file.
+ */
 template < typename T >
 class config_option_c
 : public option_value_c< T >
@@ -215,7 +256,8 @@ public:
 	 * Construct the config option with a default value.  The name
 	 * is the key in the configuration file.
 	 */
-	config_option_c( const T &default_value, const std::string &name, const std::string &desc )
+	config_option_c( const T &default_value, const std::string &name
+			, const std::string &desc )
 	: option_value_c( default_value )
 	, m_option_name( name )
 	, m_description( desc )
@@ -234,6 +276,10 @@ private:
 	bool m_config_required;
 };
 
+
+/**
+ * An option that can be set on command line usage or a configuration file.
+ */
 template < typename T >
 class shared_option_c
 : public option_value_c< T >
@@ -244,10 +290,19 @@ public:
 	shared_option_c();
 
 	virtual char option_character() const { return m_option_char; }
-	virtual const std::string & option_name() const { return m_option_name; }
-	virtual const std::string & description() const { return m_description; }
+	virtual const std::string & option_name() const
+	{
+		return m_option_name;
+	}
+	virtual const std::string & description() const
+	{
+		return m_description;
+	}
 
-	virtual bool requires_param() const { return m_requires_param; }
+	virtual bool requires_param() const
+	{
+		return usage_option_i::type_requires_param< T >();
+	}
 	virtual bool config_required() const { return m_config_required; }
 
 private:
@@ -255,168 +310,7 @@ private:
 	std::string m_description;
 
 	char m_option_char;
-	bool m_requires_param;
 	bool m_config_required;
-};
-
-
-/**
- * Templated implementation of the config_option_i interface.
- * A config_option_c class should be declared for each option
- * that can be set in the configuration file.
- * The >> ( istream& ) operator must be implemented for typename T.
- */
-template < typename T >
-class option_c
-: public option_i
-{
-public:
-	/**
-	 * Construct the config option.  The name is the key in the
-	 * configuration file.
-	 */
-	config_option_c( const std::string &name )
-	: m_name( name )
-	, m_value()
-	, m_set( false )
-	, m_error( false )
-	{}
-
-	/**
-	 * Construct the config option with a default value.  The name
-	 * is the key in the configuration file.
-	 */
-	config_option_c( const std::string &name, const T &default_value )
-	: m_name( name )
-	, m_value( default_value )
-	, m_set( true )
-	, m_error( false )
-	{}
-	virtual ~config_option_c() {}
-
-	/**
-	 * Parse the string value in the typed m_value.
-	 */
-	virtual bool parse( const std::string &str_value )
-	{
-		std::istringstream input( str_value );
-		input >> m_value;
-		m_error = input.fail();
-		m_set = ! m_error;
-	}
-
-	/**
-	 * Get the name of this config option.
-	 */
-	virtual const std::string & name() const { return m_name; }
-	/**
-	 * Return the parsed value.
-	 */
-	const T & value() const { return m_value; }
-	/**
-	 * Check if the config option was set _correctly_ in the
-	 * configuration file.
-	 */
-	virtual bool set() const { return m_set; }
-	/**
-	 * Check if the config option was set _incorrectly_ in the
-	 * configuration file.
-	 */
-	virtual bool error() const { return m_error; }
-
-	// virtual std::string doc() const = 0;
-
-private:
-	std::string m_name;
-	T m_value;
-	bool m_set;
-	bool m_error;
-};
-
-/**
- * Templated implementation of the config_option_i interface.
- * A config_option_list_c allows multiple configurations to
- * be declared for a single option.
- * The >> ( istream& ) operator must be implemented for typename T.
- */
-template < typename T >
-class option_value_list_c
-: public option_i
-{
-public:
-	typedef typename std::vector< T >::const_iterator iterator;
-private:
-	typedef std::vector< T > value_list;
-
-public:
-	/**
-	 * Construct the config option.  The name is the key in the
-	 * configuration file.
-	 */
-	config_option_list_c( const std::string &name )
-	: m_name( name )
-	, m_values()
-	, m_set( false )
-	, m_error( false )
-	{}
-	virtual ~config_option_list_c() {}
-
-	/**
-	 * Parse the string value in the typed m_value.
-	 */
-	virtual bool parse( const std::string &str_value )
-	{
-		std::istringstream input( str_value );
-		m_values.push_back( T() );
-		input >> m_values.back();
-		m_error = input.fail();
-		m_set = ! m_error;
-	}
-
-	/**
-	 * Get the name of this config option.
-	 */
-	virtual const std::string & name() const { return m_name; }
-
-	/**
-	 * Check if the config option was set _correctly_ in the
-	 * configuration file.
-	 */
-	virtual bool set() const { return m_set; }
-	/**
-	 * Check if the config option was set _incorrectly_ in the
-	 * configuration file.
-	 */
-	virtual bool error() const { return m_error; }
-
-	/**
-	 * Return the number of parsed values.
-	 */
-	int size() const { return m_values.size(); }
-	/**
-	 * [] operator to return the ith value.
-	 */
-	const T & operator [] ( int i ) const { return m_values[ i ]; }
-	/**
-	 * Return the ith value.
-	 */
-	const T & value( int i ) const { return m_values[ i ]; }
-	/**
-	 * Get the begin iterator for the list of values.
-	 */
-	iterator begin() const { return m_values.begin(); }
-	/**
-	 * Get the end iterator for the list of values.
-	 */
-	iterator end() const { return m_values.end(); }
-
-	// virtual std::string doc() const = 0;
-
-private:
-	std::string m_name;
-	value_list m_values;
-	bool m_set;
-	bool m_error;
 };
 
 
